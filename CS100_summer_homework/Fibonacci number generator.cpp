ef44ced4e4c3b3884b3dcb69c9445bff0f9480e4
nyovelt <Nyovelt@users.noreply.github.com>
2019-08-06 20:39:36 +0800

    Create Fibonacci number generator.cpp

---
+++ CS100_summer_homework/Fibonacci number generator.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+
+int main(){
+    long long number=1 ,firstNumber = 0 ,lastNumber = 1;
+
+    using namespace std;
+    cout<<"The next number is 0" << endl;
+
+    for ( int i=0; i<90; i++){
+        cout<<"The next number is " << number << endl;
+        number = firstNumber + lastNumber;
+        firstNumber = lastNumber;
+        lastNumber = number;
+    }
+}
