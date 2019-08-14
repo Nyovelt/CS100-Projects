f1e9a0967ee8eb67b7064101dd321034c62594eb
nyovelt <Nyovelt@users.noreply.github.com>
2019-08-14 14:41:59 +0800

    Update Angle-between-the-hour-and-the-minute-hand-of-a-clock.cpp

--- CS100_summer_homework/Angle-between-the-hour-and-the-minute-hand-of-a-clock.cpp
+++ CS100_summer_homework/Angle-between-the-hour-and-the-minute-hand-of-a-clock.cpp
@@ -1,14 +1,36 @@
 #include "stdio.h"
 #include "iostream"
+#include "math.h"
 using namespace std;
 char op;
-int hour,minute;
+float hour, minute;
+float minuteHand, hourHand;
 
-int main(){
-	cout << "What time is it?";
-    scanf("%d %s %d",&hour, &op, &minute);
-    if( op == ':'){
-        cout << fab((*float)hour/24 - 0.5)
+int main()
+{
+    cout << "What time is it?";
+    scanf("%f %c %f", &hour, &op, &minute);
+
+    /*if (op == ':')
+    {
+        cout << hour << ":" << minute << endl;
+    }*/
+
+    if( hour > 12){
+        hour -= 12;
     }
 
+    minuteHand = (minute / 60) * 360;
+    hourHand = ((hour / 12)* 360+(minute / 60)*30);
+    //cout << hourHand << ":" << minuteHand << endl;
+    float degrees = fabs(minuteHand - hourHand);
+    //cout << degrees << endl;
+
+    if(degrees > 180){
+        degrees = 360 - degrees;
+    }
+    //cout << degrees << endl;
+
+    printf("The angle between the hands is %.1f degrees\n", degrees);
+    getchar();
 }
