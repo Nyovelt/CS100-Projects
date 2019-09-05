2dae5201b9d97bf59268cd7323a4bdea76163100
nyovelt <Nyovelt@users.noreply.github.com>
2019-09-05 11:21:16 +0800

    Update Fibonacci-number-generator.cpp

--- CS100_summer_homework/Fibonacci-number-generator.cpp
+++ CS100_summer_homework/Fibonacci-number-generator.cpp
@@ -1,14 +1,21 @@
-using namespace std;
-#include "iostream"
+//using namespace std;
+//#include "iostream"
+#include "stdio.h"
 
-int main(){
-    long long number=1 ,firstNumber = 0 ,lastNumber = 1;
-    cout<<"The next number is 0" << endl;
+int main()
+{
+    long long int number = 1, firstNumber = 0, lastNumber = 1;
+    //cout<<"The next number is 0" << endl;
+    printf("The next number is 0 \n");
 
-    for ( int i=0; i<90; i++){
-        cout<<"The next number is " << number << endl;
+    for ( int i=1; i<89; i++){
+        printf("The next number is %lld\n", number);
+        //cout<<"The next number is " << number << endl;
         number = firstNumber + lastNumber;
         firstNumber = lastNumber;
         lastNumber = number;
     }
+
+    printf("The next number is %lld", number);
 }
+
